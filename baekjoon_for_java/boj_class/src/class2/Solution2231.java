37036d814f65dcf7c2dde726853f59aed0de29b4
tjdus2033 <tjdus2033@naver.com>
2022-07-25 00:12:02 +0900

    feat: Feat-BOJ-2231-Java

---
+++ baekjoon_for_java/boj_class/src/class2/Solution2231.java
@@ -0,0 +1,33 @@
+// CLASS_2 2231 분해합
+
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution2231 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		
+		int result = 0;
+		
+		for(int i = 0; i < N; i++) {
+			int number = i;
+			int sum = 0;
+			
+			while(number != 0) {
+				sum += number % 10;
+				number = number / 10;
+			}
+			
+			if(sum + i == N) {
+				result = i;
+				break;
+			}	
+		}
+		
+		System.out.println(result);
+	}
+}
