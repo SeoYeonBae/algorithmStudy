fd1bfdcf41ad328546d855f53e60a674c468b52b
tjdus2033 <tjdus2033@naver.com>
2022-07-24 23:43:09 +0900

    feat: Feat-BOJ-1085-Java

---
+++ baekjoon_for_java/boj_class/src/class2/Solution1085.java
@@ -0,0 +1,26 @@
+// CLASS_2 1085 직사각형에서 탈출
+
+package class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution1085 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+				String[] s = in.readLine().split(" ");
+				int x = Integer.parseInt(s[0]);
+				int y = Integer.parseInt(s[1]);
+				int w = Integer.parseInt(s[2]);
+				int h = Integer.parseInt(s[3]);
+
+				int min1 = Math.min(w - x, h - y);
+				int min2 = Math.min(x, y);
+				int result = Math.min(min1, min2);
+
+				System.out.println(result);
+
+	}
+}
