509d0f8cf34c4d47f66b3aad0a0e8d03aa92d46a
tjdus2033 <tjdus2033@naver.com>
2022-08-03 17:50:32 +0900

    feat: Add-BOJ-11050

--- src/baekjoon/class2/Main_11050_이항계수.java
+++ src/baekjoon/class2/Main_11050_이항계수.java
@@ -6,12 +6,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_11050_이항계수 {
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+		
 		System.out.println(fac(N) / (fac(N - K) * fac(K)));
 	}
 
