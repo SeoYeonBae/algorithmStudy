6d3407bcb97fc40605a26592924901fe16e21c7f
tjdus2033 <tjdus2033@naver.com>
2022-08-07 22:18:36 +0900

    Add-BOJ-10816

---
+++ src/baekjoon/Main_10816_숫자카드2.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_10816_숫자카드2 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 상근이가 가지고 있는 숫자 카드의 개수
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		
+		int M = Integer.parseInt(in.readLine());
+	}
+	
+}
