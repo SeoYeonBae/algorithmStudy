0836654ea7a562c9c5a3c2905e1a2b88fa5b7448
tjdus2033 <tjdus2033@naver.com>
2022-08-22 17:25:45 +0900

    feat: Add-BOJ-1436

---
+++ BOJ/src/baekjoon/class2/Main_1436_영화감독숌.java
@@ -0,0 +1,21 @@
+package baekjoon.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1436_영화감독숌 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int s = 666;
+		int cnt = 1;
+		while(cnt < N) {
+			s += 1;
+			if(String.valueOf(s).contains("666"))
+				cnt++;
+		}
+		System.out.println(s);
+	}
+}
