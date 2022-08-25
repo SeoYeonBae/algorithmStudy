1b0921a60a6782a804e18eb7abbdbf4195aa6ab5
tjdus2033 <tjdus2033@naver.com>
2022-08-25 21:53:06 +0900

    Update: fix-BOJ-1753

--- BOJ/src/baekjoon/Main_1753_최단경로.java
+++ BOJ/src/baekjoon/Main_1753_최단경로.java
@@ -1,5 +1,41 @@
 package baekjoon;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main_1753_최단경로 {
 
+	static class Node implements Comparable<Node>{
+		int from, to, weight;
+
+		public Node(int from, int to, int weight) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+		
+		public int compareTo(Node o) {
+			return this.weight - o.weight;
+		}
+	}
+	
+	static int V, E, K;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		V = Integer.parseInt(st.nextToken()); // 정점의 개수
+		E = Integer.parseInt(st.nextToken()); // 간선의 개수
+
+		K = Integer.parseInt(br.readLine()); // 시작 정점
+
+		br.close();
+		bw.append(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
