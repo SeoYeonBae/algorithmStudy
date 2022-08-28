efb74e8da0005e579d565ad392fc7a52a852ad67
tjdus2033 <tjdus2033@naver.com>
2022-08-29 00:10:43 +0900

    add: Add-unsolved-SWEA-Test-Solution

--- SWEA/src/test/Solution_Shuffle_O_Matic.java
+++ SWEA/src/test/Solution_Shuffle_O_Matic.java
@@ -6,6 +6,7 @@ import java.util.StringTokenizer;
 public class Solution_Shuffle_O_Matic {
 	
 	static int N;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
