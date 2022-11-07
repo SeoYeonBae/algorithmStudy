6c98544dcb0b195c5648c2c09685039b0d0c4e63
tjdus2033 <tjdus2033@naver.com>
2022-11-07 13:16:12 +0900

    feat: Add-BOJ-14719

---
+++ BOJ/src/baekjoon/Main_14719_빗물.java
@@ -0,0 +1,54 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_14719_빗물 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[][] map = new int[H][W];
+		for (int i = 0; i < H; i++) {
+			Arrays.fill(map[i], 0);
+		}
+		for (int i = 0; i < W; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			for (int j = H - 1; j >= H - num; j--) {
+				map[j][i] = 1;
+			}
+		}
+		
+		int res = 0;
+		for (int i = H - 1; i >= 0; i--) {
+			int sum = 0;
+			for (int j = 1; j < W; j++) {
+				if(map[i][j] == 0) {
+					if(map[i][j - 1] != 0) {
+						map[i][j] = -1;
+						sum++;
+					}
+				}else if(map[i][j] == 1) {
+					res += sum;
+					sum = 0;
+				}
+			}
+		}
+		for (int i = 0; i < H; i++) {
+			System.out.println(Arrays.toString(map[i]));
+		}
+		System.out.println(res);
+		br.close();
+		bw.append(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
