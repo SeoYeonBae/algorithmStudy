66b0d3a8c80e06a0898a0577d039e76aa86c2623
tjdus2033 <tjdus2033@naver.com>
2023-03-07 15:39:51 +0900

    Feat: add-level1-A+B

---
+++ Softeer/src/Level1/AplusB.java
@@ -0,0 +1,27 @@
+package Level1;
+
+import java.util.*;
+import java.io.*;
+
+
+public class AplusB
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= T; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append("Case #").append(i).append(": ").append(A + B).append("\n");
+        }
+        bw.append(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
